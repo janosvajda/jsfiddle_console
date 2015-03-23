0aa46ddf8e973ba8b907dcea9c93d21221e064d9
Janos Vajda <vajdajanos@gmail.com>
2015-03-23 12:50:09 +0000

    added html & removed dependence on the jQuery

--- console.js
+++ console.js
@@ -1,15 +1,16 @@
-/**simple jsFiddle console.log()*//
 var consoleLineTemplate = "<p style=\"margin: 1em 0 0;margin: 2px;\" class=\"console-log-template\"></p>";
     
 console = {
         log: function (value) {
+            var consoleLine = document.getElementById('console-log');
+            
             var otype=({}).toString.call(value).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
             switch(otype){
                 case 'array':
                     for (var k in value){
                         if (typeof value[k] !== 'function') {
                             var line= "key: " + k + " value: " + value[k];
-                            $("#console-log").append( $(consoleLineTemplate).html(line));
+                            consoleLine.innerHTML("<p style=\"margin: 1em 0 0;margin: 2px;\">aaaaa</p>");
                         }
                     }
                     break;
@@ -17,12 +18,12 @@ console = {
                     for (var k in value){
                         if (typeof value[k] !== 'function') {
                             var line= "key: " + k + " value: " + value[k];
-                            $("#console-log").append( $(consoleLineTemplate).html(line));
+                            consoleLine.innerHTML("<p style=\"margin: 1em 0 0;margin: 2px;\">aaaaa</p>");
+
                         }
                     }
                     break;
-                default: $("#console-log").append($(consoleLineTemplate).html(value));
+                default:  consoleLine.innerHTML("<p style=\"margin: 1em 0 0;margin: 2px;\">aaaaa</p>");
             }            
-            
         }
     };
