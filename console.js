3586bbfb7f6fa69e308de20da63844f1820b2fd0
Janos Vajda <vajdajanos@gmail.com>
2015-03-23 13:09:21 +0000

    Fixed array type.

--- console.js
+++ console.js
@@ -3,14 +3,14 @@ var consoleLineTemplate = "<p style=\"margin: 1em 0 0;margin: 2px;\" class=\"con
 console = {
         log: function (value) {
             var consoleLine = document.getElementById('console-log');
-            
+            var current_html=consoleLine.innerHTML;
             var otype=({}).toString.call(value).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
             switch(otype){
                 case 'array':
                     for (var k in value){
                         if (typeof value[k] !== 'function') {
                             var line= "key: " + k + " value: " + value[k];
-                            consoleLine.innerHTML("<p style=\"margin: 1em 0 0;margin: 2px;\">aaaaa</p>");
+                            consoleLine.innerHTML+="<p style=\"margin: 1em 0 0;margin: 2px;\">"+value+"</p>";
                         }
                     }
                     break;
@@ -18,12 +18,12 @@ console = {
                     for (var k in value){
                         if (typeof value[k] !== 'function') {
                             var line= "key: " + k + " value: " + value[k];
-                            consoleLine.innerHTML("<p style=\"margin: 1em 0 0;margin: 2px;\">aaaaa</p>");
+                            consoleLine.innerHTML+="<p style=\"margin: 1em 0 0;margin: 2px;\">"+value+"</p>";
 
                         }
                     }
                     break;
-                default:  consoleLine.innerHTML("<p style=\"margin: 1em 0 0;margin: 2px;\">aaaaa</p>");
+                default:  consoleLine.innerHTML+="<p style=\"margin: 1em 0 0;margin: 2px;\">"+value+"</p>";
             }            
         }
     };
